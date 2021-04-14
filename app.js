eab504a5ccbdffd6819102b4337d1aa6f505f800
duniaalkilany <>
2021-04-14 11:37:45 +0000

    hello from java

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+alert('hello football lovers')
+
+var nameofplayer= prompt('please enter the name of your favourite football player(ronaldo or messi)');
+
+if(nameofplayer == 'ronaldo'){document.write('ronaldo is the best player in the world');document.write('<img src="https://sport-goal.com/wp-content/uploads/2019/11/%D8%B1%D9%88%D9%86%D8%A7%D9%84%D8%AF%D9%88-%D9%85%D8%AF%D8%B1%D9%8A%D8%AF-%D8%B0%D8%A8%D9%8A%D8%A9.jpeg">');} else if (nameofplayer =='messi'){document.write('messi isnt the best player ronaldo is the best player in the world');document.write('<img src="https://img.btolat.com/news/large/1c85b65b-c252-493f-95e4-ace59adc3cfd.jpg">');}
+
+
